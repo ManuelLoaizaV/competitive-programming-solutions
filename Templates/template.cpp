cbe13e8978578f044e7685cb1168049c4c8833c1
ManuelLoaizaVasquez <manuel.loaiza.vasquez@gmail.com>
2019-09-21 21:39:15 -0500

    Plantilla que usaremos en el regional

---
+++ Templates/template.cpp
@@ -0,0 +1,30 @@
+// Your day breaks, your mind aches
+// You find that all the words of kindness linger on
+// When she no longer needs you
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+#define debug(x) cout << #x << " = " << x << endl
+#define For(i,a,b) for (int i = a; i < b; i++)
+#define roF(i,a,b) for (int i = a; i >= b; i--)
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+const int N = 1e6;
+const ll INF = 1e18;
+const ld EPS = 10e-9;
+
+int main() {
+	fastio;
+	return 0;
+}
