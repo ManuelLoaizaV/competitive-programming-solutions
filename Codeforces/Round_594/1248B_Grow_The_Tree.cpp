8b5a8bbda0bc3e77bbe0a2304c0e3c50e2ad59e8
ManuelLoaizaVasquez <manuel.loaiza.vasquez@gmail.com>
2019-11-01 15:05:36 -0500

    greedy

---
+++ Codeforces/Round_594/1248B_Grow_The_Tree.cpp
@@ -0,0 +1,53 @@
+// Your day breaks, your mind aches
+// You find that all the words of kindness linger on
+// When she no longer needs you
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+#define debug(x) cout << #x << " = " << x << endl
+#define For(i,a,b) for (int i = a; i < b; i++)
+#define roF(i,a,b) for (int i = a; i >= b; i--)
+#define pb push_back
+#define mp make_pair
+#define mt(a,b,c) mp(mp(a,b),c) 
+#define ff first
+#define ss second
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef pair<ii, int> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+const int N = 1e6;
+const ll INF = 1e18;
+const ld EPS = 10e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll random(ll a, ll b) { return uniform_int_distribution<ll> (a, b) (rng); }
+
+int main() {
+	fastio;
+	int n;
+	cin >> n;
+	ll a[n];
+	For(i,0,n) cin >> a[i];
+	sort(a, a + n);
+	ll ans = 0LL;
+	if (n == 1) ans = a[0] * a[0];
+	else {
+			int m = n / 2;
+			ll sum = 0LL;
+			For(i,0,m) sum += a[i];
+			ans += sum * sum;
+			sum = 0LL;
+			For(i,m,n) sum += a[i];
+			ans += sum * sum;
+	}
+	cout << ans << endl;
+	return 0;
+}
