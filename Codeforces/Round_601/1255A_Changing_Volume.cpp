7493fd5965983bac24f8a604c4c4ba5e8e0c7d0a
ManuelLoaizaVasquez <manuel.loaiza.vasquez@gmail.com>
2019-11-21 13:39:05 -0500

    math

---
+++ Codeforces/Round_601/1255A_Changing_Volume.cpp
@@ -0,0 +1,72 @@
+// Your day breaks, your mind aches
+// You find that all the words of kindness linger on
+// When she no longer needs you
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+#define debug(x) cout << #x << " = " << x << endl
+#define For(i,a,b) for (int i = a; i < b; i++)
+#define roF(i,a,b) for (int i = a; i >= b; i--)
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) (v).begin(),(v).end()
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+const int N = 1e6;
+const ll INF = 1e18;
+const ld EPS = 10e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll random(ll a, ll b) { return uniform_int_distribution<ll> (a, b) (rng); }
+
+int main() {
+	fastio;
+	int t;
+	cin >> t;
+	while (t--) {
+	 ll a, b;
+	 cin >> a >> b;
+	 ll ans = 0;
+	 if (a != b) {
+	 	if (a > b) {
+			ll delta = a - b;
+			ll count = delta / 5;
+			ans += count;
+			a -= 5 * count;
+			delta = a - b;
+			if (delta != 0) {
+				count = delta / 2;
+				ans += count;
+				a -= 2 * count;
+				delta = a - b;
+				if (delta != 0) ans++;
+			}
+		} else {
+			ll delta = b - a;
+			ll count = delta / 5;
+			ans += count;
+			a += 5 * count;
+			delta = b - a;
+			if (delta != 0) {
+				count = delta / 2;
+				ans += count;
+				a += 2 * count;
+				delta = b - a;
+				if (delta != 0) ans++;
+			}
+		}
+	 }
+	 cout << ans << endl;
+	}
+	return 0;
+}
