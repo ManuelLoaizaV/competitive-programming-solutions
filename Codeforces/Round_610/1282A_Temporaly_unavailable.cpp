039d77606eab7ffcc3145a695cb03410adbbf522
ManuelLoaizaVasquez <manuel.loaiza.vasquez@gmail.com>
2019-12-24 12:08:23 -0500

    math

---
+++ Codeforces/Round_610/1282A_Temporaly_unavailable.cpp
@@ -0,0 +1,52 @@
+// Your day breaks, your mind aches
+// You find that all the words of kindness linger on
+// When she no longer needs you
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+#define debug(x) cout << #x << " = " << x << endl
+#define For(i,a,b) for (int i = a; i < b; i++)
+#define roF(i,a,b) for (int i = a; i >= b; i--)
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) (v).begin(),(v).end()
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+const int N = 1e6;
+const ll INF = 1e18;
+const ld EPS = 10e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll random(ll a, ll b) { return uniform_int_distribution<ll> (a, b) (rng); }
+
+void solve() {
+	ll a, b, c, r;
+	cin >> a >> b >> c >> r;
+	ll m, M;
+	m = min(a, b);
+	M = max(a, b);
+	ll ans = M - m;
+	ll l = max(m, c - r);
+	ll R = min(M, c + r);
+	if (l <= R) ans -= (R - l);
+	cout << ans << endl;
+}
+
+int main() {
+	fastio;
+	int t;
+	cin >> t;
+	while (t--) solve();
+	return 0;
+}
