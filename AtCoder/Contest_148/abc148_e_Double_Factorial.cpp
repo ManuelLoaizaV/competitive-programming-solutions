e27c2a3d5cba5bfa1e5f06f7974e76f13a288145
ManuelLoaizaVasquez <manuel.loaiza.vasquez@gmail.com>
2019-12-24 18:41:00 -0500

    math

---
+++ AtCoder/Contest_148/abc148_e_Double_Factorial.cpp
@@ -0,0 +1,48 @@
+// Your day breaks, your mind aches
+// You find that all the words of kindness linger on
+// When she no longer needs you
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+#define debug(x) cout << #x << " = " << x << endl
+#define For(i,a,b) for (int i = a; i < b; i++)
+#define roF(i,a,b) for (int i = a; i >= b; i--)
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) (v).begin(),(v).end()
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+const int N = 1e6;
+const ll INF = 1e18;
+const ld EPS = 10e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll random(ll a, ll b) { return uniform_int_distribution<ll> (a, b) (rng); }
+
+int main() {
+	fastio;
+	ll n;
+	cin >> n;
+	if (n & 1) cout << 0 << endl;
+	else {
+		ll ans = 0;
+		ll aux = 10LL;
+		while (aux <= n) {
+			ans += n / aux;
+			aux *= 5LL;
+		}
+		cout << ans << endl;
+	}
+	return 0;
+}
