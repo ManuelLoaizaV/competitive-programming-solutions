4ec23d0cca58324ff1d4130c8b3980a7c368cd4b
ManuelLoaizaVasquez <manuel.loaiza.vasquez@gmail.com>
2020-01-22 13:18:17 -0500

    implementation

---
+++ Codeforces/Round_615/1294B_Collecting_Packages.cpp
@@ -0,0 +1,100 @@
+// Gracias, Rodolfo
+// Gracias, Roman
+// Gracias, Maxi
+// Gracias, MarcosK
+// Obrigado, Dilson
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+#define For(i,a,b) for (int i = a; i < b; i++)
+#define roF(i,a,b) for (int i = a; i >= b; i--)
+#define pb push_back
+#define mp make_pair
+#define x first
+#define y second
+#define all(v) (v).begin(),(v).end()
+#define SZ(v) ((int)v.size())
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<ii> vii;
+
+const int N = 1e6;
+const ll INF = 1e18;
+const ld EPS = 10e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll random(ll a, ll b) { return uniform_int_distribution<ll> (a, b) (rng); }
+
+bool comp(ii a, ii b) {
+	if (a.x == b.x) return a.y < b.y;
+	return a.x < b.x;
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	ii p[n];
+	For(i,0,n) cin >> p[i].x >> p[i].y;
+	sort(p, p + n, comp);
+	ii r = {0, 0};
+	string ans = "";
+	For(i,0,n) {
+		if (p[i].x >= r.x and p[i].y >= r.y) {
+			while (r.x != p[i].x) {
+				r.x++;
+				ans += 'R';
+			}
+			while (r.y != p[i].y) {
+				r.y++;
+				ans += 'U';
+			}
+		} else {
+			cout << "NO" << endl;
+			return;
+		}
+	}
+	cout << "YES" << endl;
+	cout << ans << endl;
+}
+
+int main() {
+	fastio;
+	int t = 1;
+	cin >> t;
+	while (t--) solve();
+	return 0;
+}
