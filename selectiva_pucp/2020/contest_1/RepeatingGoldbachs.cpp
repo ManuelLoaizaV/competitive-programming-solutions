695cc87999845c2072fccce1fd43a8349e8ac60e
ManuelLoaizaVasquez <manuel.loaiza.vasquez@gmail.com>
2020-05-30 23:07:21 -0500

    number theory, implementation

---
+++ selectiva_pucp/2020/contest_1/RepeatingGoldbachs.cpp
@@ -0,0 +1,101 @@
+// Gracias, Rodolfo
+// Gracias, Roman
+// Gracias, Maxi
+// Gracias, MarcosK
+// Obrigado, Dilson
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+#define For(i,a,b) for (int i = a; i < b; i++)
+#define roF(i,a,b) for (int i = a; i >= b; i--)
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) (v).begin(),(v).end()
+#define SZ(v) ((int)v.size())
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<ii> vii;
+
+const int N = 1e6;
+const ll INF = 1e18;
+const ld EPS = 10e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll random(ll a, ll b) { return uniform_int_distribution<ll> (a, b) (rng); }
+
+bool is_prime[N + 1];
+
+void sieve() {
+    for (int i = 2; i <= N; i++) is_prime[i] = true;
+    for (int i = 2; i * i <= N; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= N; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+}
+
+void solve() {
+    int x;
+    cin >> x;
+    int ans = 0;
+    while (x >= 4) {
+        int q = x;
+        while (true) {
+            int p = x - q;
+            if (is_prime[p] and is_prime[q]) {
+                x = q - p;
+                ans++;
+                break;
+            }
+            q--;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+	fastio;
+    sieve();
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
