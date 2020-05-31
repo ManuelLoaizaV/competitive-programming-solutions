84f0ee478a2200bde650383a478bae88cf565449
ManuelLoaizaVasquez <manuel.loaiza.vasquez@gmail.com>
2020-05-30 23:05:27 -0500

    dp digits

---
+++ selectiva_pucp/2020/contest_1/CountTheBits.cpp
@@ -0,0 +1,92 @@
+// Gracias, Rodolfo
+// Gracias, Roman
+// Gracias, Maxi
+// Gracias, MarcosK
+// Obrigado, Dilson
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+#define For(i,a,b) for (int i = a; i < b; i++)
+#define roF(i,a,b) for (int i = a; i >= b; i--)
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) (v).begin(),(v).end()
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<ii> vii;
+
+const int N = 1e6;
+const ll INF = 1e18;
+const ld EPS = 10e-9;
+const long long K = 1e3 + 5;
+const long long B = 130;
+const long long MOD = 1000000009LL;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll random(ll a, ll b) { return uniform_int_distribution<ll> (a, b) (rng); }
+
+long long pot[B];
+long long k, b;
+long long dp[B][K][B];
+bool done[B][K][B];
+
+long long DP(int pos, int mod, int sum) {
+    if (pos == b) {
+        if (mod == 0) return sum;
+        else return 0;
+    }
+    if (done[pos][mod][sum]) return dp[pos][mod][sum];
+    dp[pos][mod][sum] = 0LL;
+    dp[pos][mod][sum] = (DP(pos + 1, (2 * mod) % k, sum) + DP(pos + 1, (2 * mod + 1) % k, sum + 1)) % MOD;
+    done[pos][mod][sum] = true;
+    return dp[pos][mod][sum];
+}
+
+void solve() {
+    cin >> k >> b;
+    cout << DP(0, 0, 0) << endl;
+}
+
+int main() {
+	fastio;
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
