c2cd6730785dec444fa09f87f40fa45094445e12
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-08-27 18:46:16 -0500

    math

---
+++ selectiva_pucp/2020/contest_5/a.cpp
@@ -0,0 +1,110 @@
+// Gracias, Rodolfo
+// Gracias, MarcosK
+// Gracias, Graphter
+// Obrigado, Dilson
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(0);cin.tie(0)
+#define For(i,a,b) for (Long i = a; i < b; i++)
+#define roF(i,a,b) for (Long i = a; i >= b; i--)
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) (v).begin(),(v).end()
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+typedef long long Long;
+typedef long double Double;
+typedef unsigned long long ULong;
+typedef pair<Long, Long> Pair;
+typedef vector<Long> Vector;
+typedef vector<Pair> PairVector;
+
+const int N = 1e6;
+const Long INF = 1e18;
+const Double EPS = 10e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+Long random(Long a, Long b) { return uniform_int_distribution<Long> (a, b) (rng); }
+
+void solve() {
+	Long n, k;
+	cin >> n >> k;
+	Long copy = n;
+	vector<Pair> divisors;
+	for (Long p = 2; p * p <= n; p++) {
+		if (copy % p == 0) {
+			Long cnt = 0;
+			while (copy % p == 0) {
+				copy /= p;
+				cnt++;
+			}
+			divisors.push_back({p, cnt});
+		}
+	}
+	if (copy > 1) divisors.push_back({copy, 1});
+	Long total = 0;
+	int size = divisors.size();
+	for (int i = 0; i < size; i++) total += divisors[i].second;
+	vector<Long> result;
+	if (total >= k) {
+		Long last = 1LL;
+		Long rem = k;
+		For(i, 0, size) {
+			For(j, 0, divisors[i].second) {
+				if (rem > 1) {
+					result.push_back(divisors[i].first);
+					rem--;
+				} else {
+					last *= divisors[i].first;
+				}
+			}
+		}
+		result.push_back(last);
+		For(i, 0, k) {
+			if (i > 0) cout << " ";
+			cout << result[i];
+		}
+		cout << endl;
+	} else {
+		cout << -1 << endl;
+	}
+}
+
+int main() {
+	fastio;
+	int t = 1;
+	//cin >> t;
+	while (t--) solve();
+	return 0;
+}
