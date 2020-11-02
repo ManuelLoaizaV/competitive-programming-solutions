1235967fb87ac5ced24853fcadcc36814f05bb87
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-11-02 12:38:17 -0500

    binary search

---
+++ Codeforces/Round_681/1443C_The_Delivery_Dilemma.cpp
@@ -0,0 +1,96 @@
+// Gracias, Rodolfo
+// Gracias, Graphter
+// Gracias, Osman
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+using namespace std;
+#define FAST_IO ios::sync_with_stdio(0);cin.tie(NULL)
+#define For(i,a,b) for (Long i = a; i < b; i++)
+#define roF(i,a,b) for (Long i = a; i >= b; i--)
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) (v).begin(),(v).end()
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+typedef long long Long;
+typedef long double Double;
+typedef unsigned long long ULong;
+typedef pair<Long, Long> Pair;
+
+const int N = 2e5;
+const Long INF = 1e18;
+const Double EPS = 1e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+Long random(Long a, Long b) { return uniform_int_distribution<Long> (a, b) (rng); }
+
+Long a[N], b[N];
+
+bool Check(Long n, Long timer) {
+  Long walk = 0;
+  for (int i = 0; i < n; i++) {
+    if (a[i] <= timer) continue;
+    walk += b[i];
+  }
+  if (walk <= timer) return true;
+  return false;
+}
+
+void Solve(void) {
+  Long n;
+  cin >> n;
+  for (int i = 0; i < n; i++) cin >> a[i];
+  for (int i = 0; i < n; i++) cin >> b[i];
+  Long bound = 0;
+  for (int i = 0; i < n; i++) bound += b[i];
+  Long l = 1;
+  Long r = bound;
+  if (Check(n, 1)) {
+    cout << 1 << '\n';
+    return;
+  }
+  while (r - l > 1) {
+    Long mid = (l + r) / 2;
+    if (Check(n, mid)) r = mid;
+    else l = mid;
+  }
+  cout << r << '\n';
+}
+
+int main(void) {
+  FAST_IO;
+  int t = 1;
+  cin >> t;
+  while (t--) Solve();
+  return 0;
+}
