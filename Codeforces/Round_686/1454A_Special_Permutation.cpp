5cc1c511058ed1ede6767aa820758b9971c2b878
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-11-30 21:36:57 -0500

    implementation

---
+++ Codeforces/Round_686/1454A_Special_Permutation.cpp
@@ -0,0 +1,35 @@
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+#define FAST_IO ios::sync_with_stdio(0);cin.tie(NULL)
+#define debug(x) cout << #x << " = " << x << endl
+#define all(v) (v).begin(),(v).end()
+using namespace std;
+
+typedef long long Long;
+typedef long double Double;
+typedef unsigned long long ULong;
+typedef pair<Long, Long> Pair;
+
+const int N = 1e6;
+const Long INF = 1e18;
+const Double EPS = 1e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+Long random(Long a, Long b) { return uniform_int_distribution<Long> (a, b) (rng); }
+
+void Solve(void) {
+  int n;
+  cin >> n;
+  for (int i = 2; i <= n; i++) cout << i << " ";
+  cout << 1 << '\n';
+}
+
+int main(void) {
+  FAST_IO;
+  int t;
+  cin >> t;
+  while (t--) Solve();
+  return 0;
+}
