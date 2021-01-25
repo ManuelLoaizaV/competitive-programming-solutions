f3b6d516524a2d48aaf825de95f0a69ba3a6e297
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-01-25 16:00:01 -0500

    bruteforce

---
+++ Codeforces/Round_697/1475B_NewYearsNumber.cpp
@@ -0,0 +1,78 @@
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+#define FAST_IO ios::sync_with_stdio(0);cin.tie(NULL)
+#define all(v) (v).begin(),(v).end()
+using namespace std;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+typedef long long Long;
+typedef long double Double;
+typedef unsigned long long ULong;
+typedef pair<Long, Long> Pair;
+
+const int N = 1e6;
+const int M = 2021;
+const Long INF = 1e18;
+const Double EPS = 1e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+Long random(Long a, Long b) { return uniform_int_distribution<Long> (a, b) (rng); }
+
+bool is_possible[N + 1];
+
+void Solve(void) {
+  Long n;
+  cin >> n;
+  if (is_possible[n]) {
+    cout << "YES" << '\n';
+  } else {
+    cout << "NO" << '\n';
+  }
+}
+
+void Precalculate(void) {
+  for (Long i = 0; i <= M; i++) {
+    for (Long j = 0; j <= M; j++) {
+      Long nxt = (i + j) * (M - 1) + j;
+      if (nxt <= N) is_possible[nxt] = true;
+    }
+  }
+}
+
+int main(void) {
+  FAST_IO;
+  Precalculate();
+  int t = 1;
+  cin >> t;
+  while (t--) Solve();
+  return 0;
+}
+
