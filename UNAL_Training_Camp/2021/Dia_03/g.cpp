1b69207aca096d901acde86787da7062ce836154
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-04 22:08:39 -0500

    math

---
+++ UNAL_Training_Camp/2021/Dia_03/g.cpp
@@ -0,0 +1,93 @@
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+#define FAST_IO ios::sync_with_stdio(0);cin.tie(NULL)
+#define all(v) (v).begin(),(v).end()
+using namespace std;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+typedef long long Long;
+typedef long double Double;
+typedef unsigned long long ULong;
+typedef pair<int, int> Pair;
+
+const int N = 2e5;
+const Long INF = 1e18;
+const Double EPS = 1e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+Long random(Long a, Long b) { return uniform_int_distribution<Long> (a, b) (rng); }
+
+/*
+const int LG = 32 - __builtin_clz(N);
+struct SparseTable {
+  Pair st[N][LG];
+  Pair f(int x) { return {x, x}; }
+  Pair f(Pair x, Pair y) {
+    return {min(x.first, y.first), max(x.second, y.second)};
+  }
+  void Build(vector<int>& a, int n) {
+    for (int i = 0; i < n; i++) st[i][0] = f(a[i]);
+    for (int j = 1; j < LG; j++)
+      for (int i = 0; i + (1 << j) <= n; i++)
+        st[i][j] = f(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+  }
+  // O(1). Solo funciona con operadores idempotentes.
+  Pair Query(int l, int r) {
+    int range = r - l + 1;
+    int lg = 31 - __builtin_clz(range);
+    return f(st[l][lg], st[r - (1 << lg) + 1][lg]);
+  }
+} st;
+*/
+
+void Solve(void) {
+  Long n;
+  cin >> n;
+  vector<Long> a(n);
+  for (int i = 0; i < n; i++) cin >> a[i];
+  for (int i = 0; i < n - 1; i++) {
+    if (abs(a[i] - a[i + 1]) >= 2) {
+      cout << "YES\n";
+      cout << i + 1 << " " << i + 2 << '\n';
+      return;
+    }
+  }
+  cout << "NO\n";
+}
+
+int main(void) {
+  FAST_IO;
+  int t = 1;
+  cin >> t;
+  while (t--) Solve();
+  return 0;
+}
+
