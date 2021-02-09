1bfc07be41c536fc835ee567bd0bcc2b7097e05a
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-08 23:50:15 -0500

    kmp, binary search

---
+++ UNAL_Training_Camp/2021/Dia_04/e.cpp
@@ -0,0 +1,106 @@
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+#define FAST_IO ios::sync_with_stdio(0);cin.tie(NULL)
+#define all(v) (v).begin(),(v).end()
+using namespace std;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+typedef long long Long;
+typedef long double Double;
+typedef unsigned long long ULong;
+typedef pair<Long, Long> Pair;
+
+const int N = 1e6;
+const Long INF = 1e18;
+const Double EPS = 1e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+Long random(Long a, Long b) { return uniform_int_distribution<Long> (a, b) (rng); }
+
+const int ALPHABET = 26;
+vector<int> CalculatePi(const string& s) {
+  int n = s.size();
+  vector<int> pi(n, 0);
+  for (int i = 1; i < n; i++) {
+    int j = pi[i - 1];
+    while (j > 0 && s[i] != s[j]) j = pi[j - 1];
+    if (s[i] == s[j]) j++;
+    pi[i] = j;
+  }
+  return pi;
+}
+
+string t, p;
+int lower;
+
+bool Check(int len) {
+  string aux = p.substr(0, len) + char(0) + t;
+  vector<int> pi = CalculatePi(aux);
+  int cnt = 0;
+  for (int x : pi) if (x == len) cnt++;
+  return cnt >= lower;
+}
+
+void Solve(void) {
+  getline(cin, t);
+  getline(cin, p);
+  cin >> lower;
+  int n = p.size();
+  int l = 1;
+  int r = n;
+  if (Check(r)) {
+    cout << p << '\n';
+    return;
+  }
+  if (!Check(l)) {
+    cout << "IMPOSSIBLE" << '\n';
+    return;
+  }
+  while (r - l > 1) {
+    int m = (l + r) / 2;
+    if (Check(m)) {
+      l = m;
+    } else {
+      r = m;
+    }
+  }
+  for (int i = 0; i < l; i++) cout << p[i];
+  cout << '\n';
+}
+
+int main(void) {
+  FAST_IO;
+  int t = 1;
+  //cin >> t;
+  while (t--) Solve();
+  return 0;
+}
+
