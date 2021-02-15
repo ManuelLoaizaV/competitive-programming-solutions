0bb1b8752a78a6b7bb69b691c9e47e3898850186
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-15 13:18:22 -0500

    greedy

---
+++ Codeforces/Educational_Round_104/Arena.cpp
@@ -0,0 +1,69 @@
+//#pragma GCC optimize ("Ofast,unroll-loops")
+//#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#include <unistd.h>
+#define FAST_IO ios::sync_with_stdio(0);cin.tie(NULL)
+#define all(v) (v).begin(),(v).end()
+using namespace std;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+typedef long long Long;
+typedef long double Double;
+typedef unsigned long long ULong;
+typedef pair<Long, Long> Pair;
+
+const int N = 1e6;
+const Long INF = 1e18;
+const Double EPS = 1e-9;
+
+Long GetBit(Long mask, Long bit) { return (mask >> bit) & 1LL; }
+void TurnOn(Long& mask, Long bit) { mask = mask | (1LL << bit); }
+void TurnOff(Long& mask, Long bit) { mask = mask & (~(1 << bit)); }
+
+void Solve(void) {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  int ans = 0;
+  for (int i = 0; i < n; i++) cin >> a[i];
+  sort(a.begin(), a.end());
+  for (int i = 0; i < n; i++) {
+    if (a[0] < a[i]) ans++;
+  }
+  cout << ans << '\n';
+}
+
+int main(void) {
+  FAST_IO;
+  int t = 1;
+  cin >> t;
+  while (t--) Solve();
+  return 0;
+}
+ 
